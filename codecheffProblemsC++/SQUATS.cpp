14dbbf227d3d8d487521042185fd666b50f557d1
Rishikesh Raj <rishi581b@gmail.com>
2022-11-23 20:04:58 +0530

    23

---
+++ codecheffProblemsC++/SQUATS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int set;
+        cin >> set;
+        cout << set * 15 << endl;
+    }
+}
