f0c52db05732991437007a8cf178096a50745d8d
Rishikesh Raj <rishi581b@gmail.com>
2022-11-23 20:10:27 +0530

    24

---
+++ codecheffProblemsC++/MONOPOLY.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a > b + c)
+        {
+            cout << "yes" << endl;
+        }
+        else if (b > a + c)
+        {
+            cout << "yes" << endl;
+        }
+        else if (c > a + b)
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+    }
+}
