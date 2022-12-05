9da0d3ef55a6e7aa167a17f6c303da8d9c77c075
Rishikesh Raj <rishi581b@gmail.com>
2022-12-05 18:09:39 +0530

    77

---
+++ codecheffProblemsC++/CNDLOVE.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n], sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+            sum = sum + arr[i];
+        }
+        if (sum % 2 != 0)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+}
