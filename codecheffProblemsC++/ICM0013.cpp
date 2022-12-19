7ced5f8ae3a8681d35cfc80769cbaf8a9da49b81
Rishikesh Raj <rishi581b@gmail.com>
2022-12-19 17:44:01 +0530

    113

---
+++ codecheffProblemsC++/ICM0013.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << n << "\n";
+    }
+}
