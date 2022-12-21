c4282b863d57ed98850499db6518373faa1564d4
Rishikesh Raj <rishi581b@gmail.com>
2022-12-21 18:25:06 +0530

    128

---
+++ codecheffProblemsC++/CENS20D.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n], count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if ((arr[i] & arr[j]) == arr[i])
+                {
+                    count = count + 1;
+                }
+            }
+        }
+        cout << count << "\n";
+    }
+}
