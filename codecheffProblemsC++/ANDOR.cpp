ba8f172654d812f74347821416f8c1f2a9fa56ff
Rishikesh Raj <rishi581b@gmail.com>
2022-12-22 14:52:41 +0530

    136

---
+++ codecheffProblemsC++/ANDOR.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define ll unsigned long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll x;
+        cin >> x;
+        cout << "0 " << x << "\n";
+    }
+}
