f2b855804838d814853db718f74684876409d288
Rishikesh Raj <rishi581b@gmail.com>
2022-12-29 20:00:34 +0530

    159

---
+++ codecheffProblemsC++/BALREV.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        sort(s.begin(), s.end());
+        cout << s << "\n";
+    }
+}
